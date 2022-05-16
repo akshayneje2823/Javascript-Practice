2a7fcf60008a0075b1c92b501525f21eecdc381b
Akshay Neje <akshayneje2823@gmail.com>
2022-05-16 16:45:49 +0530

    Done

--- nested.js
+++ nested.js
@@ -2,3 +2,12 @@ for(var a =1; a<=100; a = a+10){
     for(var b = a; b<a+10; b++)
     console.log(b);
 }
+
+let a = [1,2,3,4,5,6,67,8,9]
+
+
+for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    
+}
+console.log(ele)
